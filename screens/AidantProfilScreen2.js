f17618205399bfb8ff0cb9616878cca26a9ba417
Marion <marionlaurin@hotmail.com>
2023-05-15 16:20:39 +0200

    updateteam

---
+++ screens/AidantProfilScreen2.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AidantProfilScreen2() {
+  return (
+    <View>
+      <Text>AidantProfilScreen2</Text>
+    </View>
+  )
+}
---
+++ screens/AidantProfilScreen3.js
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function AidantProfilScreen3() {
+  return (
+    <View>
+      <Text style={styles.title}>Mes Talents</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  title: {
+    color: '#785C83',
+  }
+})
