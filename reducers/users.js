97ca6193ce9a4215d92b9a91657e5a3b7a2752b3
Camille Bonnet <c.bonnet93@gmail.coom>
2023-05-16 15:46:53 +0200

    save

--- reducers/users.js
+++ reducers/users.js
@@ -1,7 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    value: { token: null, email: null },
+    value: { 
+      token: null, 
+      email: null, 
+      selectedImageAidant: null,
+      nameAidant: null,
+      firstNameAidant: null,
+      phoneAidant: null,
+      ageAidant: null,
+      sexeAidant: null,
+      addressAidant: null,
+      zipAidant: null,
+      cityAidant: null,
+      ratebyHourAidant: null,
+      carAidant: null,
+      introBioAidant: null,
+      longBioAidant: null,
+      abilitiesAidant: null,
+    },
   };
   
 export const userSlice = createSlice({
@@ -16,9 +33,25 @@ reducers: {
     state.value.token = null;
     state.value.email = null;
     },
+    updateAidant: (state) => {
+      state.value.selectedImageAidant = action.payload.selectedImageAidant;
+      state.value.nameAidant = action.payload.nameAidant;
+      state.value.firstNameAidant = action.payload.firstNameAidant;
+      state.value.phoneAidant = action.payload.phoneAidant;
+      state.value.ageAidant = action.payload.ageAidant;
+      state.value.sexeAidant = action.payload.sexeAidant;
+      state.value.addressAidant = action.payload.addressAidant;
+      state.value.zipAidant = action.payload.zipAidant;
+      state.value.cityAidant = action.payload.cityAidant;
+      state.value.ratebyHour = action.payload.ratebyHour;
+      state.value.car = action.payload.car;
+      state.value.introBioAidant = action.payload.introBioAidant;
+      state.value.longBioAidant = action.payload.longBioAidant;
+      state.value.abilitiesAidant = action.payload.abilitiesAidant;
+      },
 
 },
 });
 
-export const { login, logout } = userSlice.actions;
+export const { login, logout, updateAidant } = userSlice.actions;
 export default userSlice.reducer;
