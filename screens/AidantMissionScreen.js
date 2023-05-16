f878c54bd974ab0b6b3b463786e7191b40a66cb8
Camille Bonnet <c.bonnet93@gmail.coom>
2023-05-16 11:47:10 +0200

    profil aidant

--- screens/AidantMissionScreen.js
+++ screens/AidantMissionScreen.js
@@ -1,17 +1,40 @@
 import React, { useState } from 'react'
-import {  View, Text, TextInput, StyleSheet, Image } from 'react-native'
+import { Switch, View, Text, TextInput, StyleSheet, Image } from 'react-native'
 import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function AidantMissionScreen() {
+
+  //text rempli
+  //nom de l'aidant
+  const [nameAidant, setnameAidant] = useState("");
+  //prénom de l'aidant
+  const [firstNameAidant, setfirstNameAidant] = useState("");
+  //téléphone de l'aidant
+  const [phoneAidant, setphoneAidant] = useState("");
+  //âge de l'aidant
+  const [ageAidant, setageAidant] = useState("");
+  //sexe de l'aidant
+  const [sexeAidant, setsexeAidant] = useState("");
+  //adresse de l'aidant
+  const [addressAidant, setaddressAidant] = useState("");
+  //code postal de l'aidant
+  const [zipAidant, setzipAidant] = useState("");
+  //ville de l'aidant
+  const [cityAidant, setcityAidant] = useState("");
+  //tarif horaire de l'aidant
+  const [ratebyHour, setratebyHour] = useState("");
 
   //menu dropdown pour le sexe
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
-    {label: 'Apple', value: 'apple'},
-    {label: 'Banana', value: 'banana'}
+    {label: 'Femme', value: 'femme'},
+    {label: 'Homme', value: 'homme'},
+    {label: 'Indifférent', value: 'indifférent'},
   ]);
 
+  //toggle pour le permis
+  const [car, setcar] = useState(false);
 
   return (
     <View style={styles.container}>
@@ -30,41 +53,88 @@ export default function AidantMissionScreen() {
 
     <View style={styles.containerInput}>
       <Text>Nom</Text>
-      <TextInput style={styles.input} onChangeText={text => setTextInputValue(text)} placeholder="Nom" />
+      <TextInput style={styles.input} onChangeText={text => setnameAidant(text)} placeholder="Nom" />
     </View>
 
     {/* prénom de l'aidant */}
     <View style={styles.containerInput}>
       <Text>Prénom</Text>
-      <TextInput style={styles.input} onChangeText={text => setTextInputValue(text)} placeholder="Prénom" />
+      <TextInput style={styles.input} onChangeText={text => setfirstNameAidant(text)} placeholder="Prénom" />
     </View>
 
     {/* téléphone de l'aidant */}
     <View style={styles.containerInput}>
       <Text>Téléphone</Text>
-      <TextInput style={styles.input} onChangeText={text => setTextInputValue(text)} placeholder="Téléphone" />
+      <TextInput style={styles.input} onChangeText={text => setphoneAidant(text)} placeholder="Téléphone" />
     </View>
 
-    {/* âge de l'aidant */}
-    <View style={styles.containerInput}>
-      <TextInput style={styles.input} onChangeText={text => setTextInputValue(text)} placeholder="YYYY" />
-      <Text>Année de naissance</Text>
+      {/* adresse de l'aidant */}
+      <View style={styles.containerInput}>
+        <Text>Adresse</Text>
+        <TextInput style={styles.input} onChangeText={text => setaddressAidant(text)} placeholder="Adresse" />
+      </View>
+
+    <View style={styles.doubleInput}>
+
+      {/* code postal de l'aidant */}
+      <View style={styles.smallcontainerInput}>
+      <Text>Code Postal</Text>
+        <TextInput style={styles.codePostal} onChangeText={text => setzipAidant(text)} placeholder="CP" />
+      </View>
+
+      {/* code postal de l'aidant */}
+      <View style={styles.smallcontainerInput}>
+      <Text>Ville</Text>
+        <TextInput style={styles.city} onChangeText={text => setcityAidant(text)} placeholder="Ville" />
+      </View>
+
     </View>
 
-     {/* sexe de l'aidant */}
-    <View style={styles.containerInput}>
-      <View>
-        <DropDownPicker
-        open={open}
-        value={value}
-        items={items}
-        setOpen={setOpen}
-        setValue={setValue}
-        setItems={setItems}
+    <View style={styles.doubleInput}>
+
+      {/* âge de l'aidant */}
+      <View style={styles.smallcontainerInput}>
+      <Text>Naissance</Text>
+        <TextInput style={styles.smallinput} onChangeText={text => setageAidant(text)} placeholder="AAAA" />
+      </View>
+
+      {/* sexe de l'aidant */}
+      <View style={styles.smallcontainerInput}>
+          <Text>Sexe</Text>
+          <DropDownPicker style={{width: 120, marginLeft: 15, borderColor: '#5ABAB6'}} placeholderStyle={{color: "grey"}} disabledStyle={{opacity: 0.5}}
+          open={open}
+          value={value}
+          items={items}
+          placeholder="Sexe"
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+          dropDownContainerStyle={{ width: 120, marginLeft: 15, marginBottom: 15, borderColor: '#5ABAB6' }}
+          onSelectItem={(item) => {
+            setsexeAidant(item)
+          }}
+          />
+      </View>
+
+
+    </View>
+
+
+     {/* tarif horaire l'aidant */}
+     <View style={styles.tarifcontainerInput}>
+      <Text>Tarif horaire</Text>
+        <TextInput style={styles.city} onChangeText={text => setratebyHour(text)} placeholder="Tarif/heure" />
+      </View>
+
+      {/* permis l'aidant */}
+     <View style={styles.tarifcontainerInput}>
+        <Text>Permis B</Text>
+        <Switch style={{marginLeft: 30}}
+        value={car}
+        onValueChange={(value) => setcar(value)}
+        trackColor={{ false: '#D9D9D9', true: '#5ABAB6' }}
         />
       </View>
-      <Text>Sexe</Text>
-    </View>
 
     </View>
   )
@@ -87,13 +157,55 @@ const styles = StyleSheet.create({
       borderBottomColor: '#5ABAB6',
       borderBottomWidth: 1,
     },
+    smallinput: {
+      width: 70,
+      height:26,
+      borderBottomColor: '#5ABAB6',
+      borderBottomWidth: 1,
+      marginLeft: 25,
+      marginRight: 15
+    },
+    codePostal:  {
+      width: 50,
+      height:26,
+      borderBottomColor: '#5ABAB6',
+      borderBottomWidth: 1,
+      marginLeft: 25,
+      marginRight: 25
+    },
+    city:  {
+      width: 120,
+      height:26,
+      borderBottomColor: '#5ABAB6',
+      borderBottomWidth: 1,
+      marginLeft: 15,
+      marginRight: 15
+    },
     containerInput: {
       fontSize: 13,
       flexDirection: "row",
       alignItems:"center",
       justifyContent: "space-between",
-      marginLeft:25,
+      marginLeft:20,
       marginRight:25,
       marginBottom:25,
+    },
+    tarifcontainerInput: {
+      fontSize: 13,
+      flexDirection: "row",
+      alignItems:"center",
+      marginLeft:20,
+      marginRight:25,
+      marginBottom:25,
+    },
+    doubleInput: {
+      flexDirection: "row",
+      marginLeft:20,
+      marginRight:25,
+      marginBottom:25,
+    },
+    smallcontainerInput: {
+      flexDirection: "row",
+      alignItems:"center", 
     },
     })
