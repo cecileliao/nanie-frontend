03107cb664ea25b626d7f3a98f443ac6b1d7c212
Camille Bonnet <c.bonnet93@gmail.coom>
2023-05-17 16:26:51 +0200

    save

--- screens/AidantDisplayProfilScreen.js
+++ screens/AidantDisplayProfilScreen.js
@@ -1,13 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
 import React from 'react'
 
 export default function AidantDisplayScreen() {
   return (
     <View>
       <Text>AidantProfilScreen1</Text>
-      <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button}>
             <Text style={styles.buttonText}>Suivant</Text>
-          </TouchableOpacity>
+        </TouchableOpacity>
     </View>
   )
 }
+
+//mise en place méthode Dimension pour mettre en % pour faire fonctionner le KeyboardAvoidingView
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center', // par défaut justify-content: 'flex-start', pour que le padding du keyboardavoiding fonctionne il faut le mettre sur flex-end ou center
+      },
+    buttonText: {
+        color: 'white',
+        fontFamily: "Manrope",
+      },
+        button: {
+        backgroundColor: '#5ABAB6',
+        padding: 10,
+        borderRadius: 8,
+        marginTop: 8,
+        width: 120,
+        textAlign: 'center',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    }); 
