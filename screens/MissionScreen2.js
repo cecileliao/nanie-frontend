5f19be0d66d05f85063498e5e75f688689d52331
Camille Bonnet <c.bonnet93@gmail.coom>
2023-05-24 17:09:46 +0200

    save

--- screens/MissionScreen2.js
+++ screens/MissionScreen2.js
@@ -1,7 +1,26 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'   
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 
 export default function MissionScreen2() {
+
+  const BACKEND_ADDRESS = '192.168.10.128:3000';
+  const user = useSelector((state) => state.user.value);
+
+  useEffect(() => {
+    console.log('search', user.searchResult[0].token)
+    fetch(`http://${BACKEND_ADDRESS}/DetailsMission/${user.mission._id}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log('$$$', data);
+        // if (data.result)  {
+        // dispatch(displayProfil)
+      });
+  }, []);
+
+
+
   return (
     <View>
       <Text>MissionScreen2</Text>
@@ -11,3 +30,4 @@ export default function MissionScreen2() {
     </View>
   )
 }
+
